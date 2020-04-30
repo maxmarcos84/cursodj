0d15115676f06a8d309d8db472d7a21a4a67e45c
maxmarcos84 <maxmarcos84@gmail.com>
2020-04-30 17:40:55 -0300

    login

--- generales/urls.py
+++ generales/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 from generales.views import Home
 
 urlpatterns = [
     path('',Home.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='generales/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='generales/login.html'), name='logout'),
     
 ]
